9be62b29a88358ef81d6862d391493367a08a390
zj <mzjzhaojun@163.com>
2017-05-09 09:50:06 +0800

    5.9

--- src/main/java/com/yt/app/advice/DataSourceAdvice.java
+++ src/main/java/com/yt/app/advice/DataSourceAdvice.java
@@ -3,9 +3,10 @@ package com.yt.app.advice;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,6 @@ import com.yt.app.frame.datasource.DynamicDataSource;
 @Aspect
 @Component
 public class DataSourceAdvice {
-
 	@Autowired
 	private DynamicDataSource dataSourceHolder;
 
@@ -26,11 +26,9 @@ public class DataSourceAdvice {
 	public void aspect() {
 	}
 
-	@Around("aspect()")
-	public Object Around(ProceedingJoinPoint joinPoint) throws Throwable {
+	@Before("aspect()")
+	public void Before(JoinPoint joinPoint) throws Throwable {
 		Annotation annotation = null;
-		Object[] args = joinPoint.getArgs();
-
 		Method me = ((MethodSignature) joinPoint.getSignature()).getMethod();
 		annotation = me.getAnnotations().length > 0 ? me.getAnnotations()[0] : null;
 		if (annotation == null) {
@@ -38,7 +36,7 @@ public class DataSourceAdvice {
 			Method mes = proxyMethod.getDeclaringClass().getMethod(proxyMethod.getName(), proxyMethod.getParameterTypes());
 			annotation = mes.getAnnotations().length > 0 ? mes.getAnnotations()[0] : null;
 			if (annotation == null) {
-				return joinPoint.proceed(args);
+				dataSourceHolder.markMaster();
 			}
 		}
 		if (annotation.annotationType().equals(DataSourceAnnotation.class)) {
@@ -49,7 +47,11 @@ public class DataSourceAdvice {
 				dataSourceHolder.markMaster();
 			}
 		}
-		return joinPoint.proceed(args);
+	}
+
+	@After("aspect()")
+	public void After(JoinPoint joinPoint) throws Throwable {
+		dataSourceHolder.markRemove();
 	}
 
 }
